3502468be626f8812a1cc492bfd2a6a6ad7d86b8
Rails udemy course <nitrous850@gmail.com>
2016-06-13 07:04:26 +0000

    create show action and gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,10 @@ end
 else
       render 'edit'
     end
-
   end
-
+def show
+  @user = User.find(params[:id])
+end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
