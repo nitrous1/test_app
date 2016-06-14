bfd5fba3afc6090ae9563594724a767ea12587aa
Rails udemy course <nitrous850@gmail.com>
2016-06-14 12:08:49 +0000

    Add admin requirement to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,8 @@
 class CategoriesController < ApplicationController
-def index
+before_action :require_admin, except: [:index, :show]
+
+
+  def index
  @categories = Category.paginate(page: params[:page], per_page: 5)
 end
 
@@ -20,8 +23,15 @@ end
 
  end
 
-end
 private
   def category_params
     params.require(:category).permit(:name)
   end
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "only admin can perform that action"
+      redirect_to categories_path
+
+    end
+  end
+end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,17 +3,26 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
   @category = Category.create(name: "sports")
+    @user = User.create(username: "johnsnow1", email: "sunny_92@rocketmail.com", password: "raone2010", admin: true)
   end
   test "should get categories index" do
     get :index
     assert_response :success
   end
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  test "should redirect create when admin noy logged in" do
+    assert_no_difference 'Category.count' do
+      post :create,category: { name:"sports" }
+
+    end
+    assert_redirected_to categories_path
   end
 end
